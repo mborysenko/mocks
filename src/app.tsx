1f3353a4ce4be6a7096a16d7d58eef0615bf2da8
mborysenko <mborysenko@sdl.com>
2018-04-06 11:34:01 +0300

    Created initial web application

---
+++ src/app.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+import "styles";
+/* React Router stuff */
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+/* Redux stuff */
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {routerMiddleware, routerReducer} from "react-router-redux";
+import {Provider} from "react-redux";
+
+const history = createHistory();
+const middleware = routerMiddleware(history as any);
+
+const store = createStore(combineReducers({
+    routing: routerReducer
+}), applyMiddleware(middleware));
+
+export interface IAppProperties {
+}
+
+export interface IAppState {
+    context?: any;
+}
+
+export class App extends React.Component<IAppProperties, IAppState> {
+    public render(): JSX.Element {
+        return <Provider store={store}>
+            <Router>
+                <div>
+                    <h1>Real Time Board</h1>
+                </div>
+            </Router>
+        </Provider>;
+    }
+}
+
+export {
+    App as default
+}
